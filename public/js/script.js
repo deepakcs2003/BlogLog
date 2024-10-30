3b4a31da0a16e63e053637b992c747b4abdc95b0
deepakcs2003 <135646985+deepakcs2003@users.noreply.github.com>
2024-10-30 14:23:20 +0530

    i fix the header to make is more user friedly

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
   const darkModeToggle = document.getElementById("dark-mode-toggle");
-  const icon=document.getElementById('dark-icon');
+  const icon = document.getElementById('dark-icon');
   const body = document.body;
+  const header = document.querySelector('.header');
   
+  // Initial setup based on saved dark mode preference
   const savedDarkMode = localStorage.getItem('darkMode');
   if (savedDarkMode == 'enabled') {
-
     body.classList.add('dark-mode');
-    icon.classList.add('fa-sun')
+    icon.classList.add('fa-sun');
+    header.style.backgroundColor = '#1a1a1a'; // Set dark background for header
   } else {
     body.classList.remove('dark-mode');
-    icon.classList.add('fa-moon')
+    icon.classList.add('fa-moon');
+    header.style.backgroundColor = 'white'; // Set light background for header
   }
   
-
   if (darkModeToggle) {
-
     darkModeToggle.addEventListener("click", function() {
         body.classList.toggle("dark-mode");
       
@@ -23,20 +24,20 @@ document.addEventListener('DOMContentLoaded', function(){
           localStorage.setItem('darkMode','enabled');
           icon.classList.remove('fa-moon');
           icon.classList.add('fa-sun');
+          header.style.backgroundColor = '#1a1a1a'; // Change to dark background
         } else {
           localStorage.setItem('darkMode','disabled');
           icon.classList.remove('fa-sun');
           icon.classList.add('fa-moon');
+          header.style.backgroundColor = '#FAF5EE'; // Change to light background
         }
     });
-}
-
+  }
   const allButtons = document.querySelectorAll('.searchBtn');
   const searchBar = document.querySelector('.searchBar');
   const searchInput = document.getElementById('searchInput');
   const searchClose = document.getElementById('searchClose');
 
-  
   for (var i = 0; i < allButtons.length; i++) {
     allButtons[i].addEventListener('click', function() {
       searchBar.style.visibility = 'visible';
@@ -51,6 +52,4 @@ document.addEventListener('DOMContentLoaded', function(){
     searchBar.classList.remove('open');
     this.setAttribute('aria-expanded', 'false');
   });
-
-
 });
